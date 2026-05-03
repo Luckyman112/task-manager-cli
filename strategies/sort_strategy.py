81c2180911c84bbd3714619b78cff4aa2675507a
LuckyMan00711 <77516744+LuckyMan00711@users.noreply.github.com>
2026-05-03 03:16:01 +0300

    Update sort_strategy.py

--- strategies/sort_strategy.py
+++ strategies/sort_strategy.py
@@ -1,22 +1,16 @@
 from typing import List
 from models.itask import ITask
-from strategies.isort_strategy import ISortStrategy #Путь до файла с интерфейсом
+from strategies.isort_strategy import ISortStrategy 
 
-# 1. Стратегия сортировки по дате
 class SortByDateStrategy(ISortStrategy):
     def sort(self, tasks: List[ITask]) -> List[ITask]:
-        # Сортируем задачи по дате дедлайна. 
-        # Если даты нет (None), отправляем задачу в конец списка.
         return sorted(tasks, key=lambda task: task.due_date if task.due_date else "9999-12-31")
 
-# 2. Стратегия сортировки по приоритету
 class SortByPriorityStrategy(ISortStrategy):
     def sort(self, tasks: List[ITask]) -> List[ITask]:
-        # Маппинг в числа для сортировки 1 - наивысший
         priority_map = {'High': 1, 'Medium': 2, 'Low': 3}
         return sorted(tasks, key=lambda task: priority_map.get(getattr(task, 'priority', 'Low'), 4))
 
-# 3. Класс контекст
 class TaskSorter:
     def __init__(self, strategy: ISortStrategy):
         self._strategy = strategy
